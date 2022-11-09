7097706e6896ee49190e60829f29d2496f326bdb
JuanManuelCastelo <79878352+JuanManuelCastelo@users.noreply.github.com>
2022-11-08 22:58:13 -0300

    Create while.java

---
+++ while.java
@@ -0,0 +1,16 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+     var numeroWhile = -5;
+
+     while (numeroWhile < 3) {
+
+         System.out.println("El numero es " + numeroWhile);
+         numeroWhile++;
+     }
+    }
+
+
+
+}
