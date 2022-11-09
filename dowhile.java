7a47b480cd7d834435f5366ac98043e87f9baedd
JuanManuelCastelo <79878352+JuanManuelCastelo@users.noreply.github.com>
2022-11-08 23:00:58 -0300

    Create dowhile.java

---
+++ dowhile.java
@@ -0,0 +1,16 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+     var numeroWhile = 3;
+
+     do {
+         System.out.println("El numero es " + numeroWhile);
+         numeroWhile++;
+     } while (numeroWhile < 3);
+
+    }
+
+
+
+}
